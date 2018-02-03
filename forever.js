ae7b361a92a9b2575655798e6ebd0b9c5bb6cf4e
clubmax27 <Glados.Plague@gmail.com>
2018-02-03 21:39:26 +0100

    Added a forever.js to run forever the bot

---
+++ forever.js
@@ -0,0 +1,14 @@
+var forever = require('forever-monitor');
+
+var child = new (forever.Monitor)('bot.js',
+{
+  max: 3,
+  silent: false,
+  args: []
+});
+
+child.on('exit', function () {
+  console.log('your-filename.js has exited after 3 restarts');
+});
+
+child.start();
