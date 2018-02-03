68ecfc002849ca9f993657a486582dc8b3660fe2
Aposition <36055675+Aposition@users.noreply.github.com>
2018-02-03 16:31:45 -0500

    Fixed the repeat in the code.

--- BOT1.js
+++ BOT1.js
@@ -90,10 +90,6 @@ client.on('message', function(message)
       break;
 
     case "WANNA FUCK?":
-      message.channel.send("Yeah?");
-      break;
-
-    case "WANNA FUCK?":
       message.channel.send("You bet!");
       break;
 
