1ad098f4ea06bbf2259af417fdcd113342069993
clubmax27 <Glados.Plague@gmail.com>
2018-03-10 08:36:52 +0100

    Added a hug and derpibooru command

--- commands/DERPIBOORU.js
+++ commands/DERPIBOORU.js
@@ -6,7 +6,7 @@ exports.run = function(client, message)
   const command = args.shift().toLowerCase();
   if(message.channel.nsfw || message.channel.type === "dm") //If the channel is NSFW or was send in PM
   {
-    getClopList(args[0], message); //call getClopList with the given tags and the message object
+    getClopList(message); //call getClopList with the given tags and the message object
   }
   else
   {
@@ -16,28 +16,28 @@ exports.run = function(client, message)
 
 function setClopList(data, message)
 {
-  var clopList = data.split("<post "); //Split the differents images found in the clopList array
+  var clopList = data.split("\"full\""); //Split the differents images found in the clopList array
   for(var i = 1; i <= clopList.length - 1; i++) //For every element of the array
   {
-    var start_url = clopList[i].indexOf("file_url=") + 10; //Find the start of the URL
-    var end_url = clopList[i].indexOf("parent_id=") - 2; //Find the end of the URL
+    var start_url = clopList[i].indexOf("\"full\"") + 5; //Find the start of the URL
+    var end_url = clopList[i].indexOf("\"},"); //Find the end of the URL
     clopList[i] = clopList[i].substring(start_url,end_url); //Replace the whole text by just the URL
   }
   clopList.shift(); //Remove the first element
   if(clopList[0] === undefined)
   {
-    message.channel.send("No porn could be found with your stupid ass tag");
+    message.channel.send("No porn could be found (you should probably tell @Gaiben#7736 about it)");
     return;
   }
   message.channel.send("Here you go, some not so family friendly porn");
-  message.channel.send(clopList[Math.floor(Math.random() * clopList.length)]); //Send a random element of the array
+  message.channel.send("https://" + clopList[Math.floor(Math.random() * clopList.length)]); //Send a random element of the array
   return true;
 }
 
-function getClopList(tag, message)
+function getClopList(message)
 {
   var r34 = new XMLHttpRequest();
-  r34.open("GET","https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&tags=" + tag,true);
+  r34.open("GET","https://derpibooru.org/tags/explicit.json?key=8PJq9sL9kzLzrYe1ycxh", true);
   r34.send(null); //send a GET request to the URL above
   r34.addEventListener("readystatechange", function()
   {
